cc44e9813f076f19b9d1313ab535a38905eb73a4
tla001 <1414419047@qq.com>
2016-11-24 09:07:28 +0800

    build

---
+++ Programs/inc/Common.h
@@ -0,0 +1,25 @@
+/*
+ * Common.h
+ *
+ *  Created on: 2016Äê11ÔÂ24ÈÕ
+ *      Author: tla001
+ */
+
+#ifndef INC_COMMON_H_
+#define INC_COMMON_H_
+
+extern "C" {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+}
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+using namespace __gnu_cxx;
+
+#endif /* INC_COMMON_H_ */
---
+++ Programs/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2016Äê11ÔÂ24ÈÕ
+ *      Author: tla001
+ */
+#include "Common.h"
+
+int main() {
+	string a = to_string(123);
+	cout << a << endl;
+	return 0;
+}
+
